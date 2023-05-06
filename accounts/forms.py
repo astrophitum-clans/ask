37f2d7733fada9e143599eefb566824bf3f05ea3
Michael Smith <astrophitum.clans@gmail.com>
2023-05-06 19:36:55 +0300

    create project, docker settings, add accounts app

---
+++ accounts/forms.py
@@ -0,0 +1,24 @@
+from django.contrib.auth import get_user_model
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+
+class CustomUserCreationForm(UserCreationForm):
+    """Custom User creation form"""
+
+    class Meta:
+        model = get_user_model()
+        fields = (
+            'email',
+            'username',
+        )
+
+
+class CustomUserChangeForm(UserChangeForm):
+    """Custom User change form"""
+
+    class Meta:
+        model = get_user_model()
+        fields = (
+            'email',
+            'username',
+        )
